5ad238ef47dcd0cbf9327944214396861431f4a6
Damien Lindley <damien@dcpendleton.plus.com>
2017-05-01 19:00:36 +0100

    Somewhat improved readability with comments. Clarified translator comments in relation to docstrings. Changed formatted strings so they should work with the translation system.

--- addon/globalPlugins/clipspeak/__init__.py
+++ addon/globalPlugins/clipspeak/__init__.py
@@ -1,9 +1,10 @@
-#Clipspeak
-#An addon to monitor and speak messages relating to clipboard operations.
-#By: Damien Lindley
-#Created: 19th April 2017
-#This file is covered by the GNU General Public License.
-#See the file COPYING for more details.
+# Clipspeak
+# An addon to monitor and speak messages relating to clipboard operations
+# By: Damien Lindley
+# Created: 19th April 2017
+
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
 
 import globalPluginHandler
 import ui
@@ -16,9 +17,9 @@ from controlTypes import *
 
 import clipboard_monitor
 
-#Constants
+# Constants:
 
-#Clipboard content: What are we working with?
+# Clipboard content: What are we working with?
 cc_none=0
 cc_text=1
 cc_read_only_text=2
@@ -26,47 +27,65 @@ cc_file=3
 cc_list=4
 cc_other=5
 
-#Clipboard mode: What are we doing?
+# Clipboard mode: What are we doing?
 cm_none=0
 cm_cut=1
 cm_copy=2
 cm_paste=3
 
-#Not strictly clipboard, but...
+# Not strictly clipboard, but...
 cm_undo=4
 cm_redo=5
 cm_select_all=6
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
-	#Script functions.
+	# Script functions:
 
 	def script_cut(self, gesture):
 		if self.process_input(gesture): return
 		self.speak_appropriate_message(cm_cut)
 
+	# Translators: Documentation for cut script.
+	script_cut.__doc__=_("Cut selected item to clipboard, if appropriate.")
+
 	def script_copy(self, gesture):
 		if self.process_input(gesture): return
 		self.speak_appropriate_message(cm_copy)
+
+	# Translators: Documentation for copy script.
+	script_copy.__doc__=_("Copy selected item to clipboard, if appropriate.")
 
 	def script_paste(self, gesture):
 		if self.process_input(gesture): return
 		if not self.__clipboard.valid_data(): return
 		self.speak_appropriate_message(cm_paste)
 
+	# Translators: Documentation for paste script.
+	script_paste.__doc__=_("Paste item from clipboard, if appropriate.")
+
 	def script_undo(self, gesture):
 		if self.process_input(gesture): return
 		self.speak_appropriate_message(cm_undo)
+
+	# Translators: Documentation for undo script.
+	script_undo.__doc__=_("Undo operation.""")
 
 	def script_redo(self, gesture):
 		if self.process_input(gesture): return
 		self.speak_appropriate_message(cm_redo)
 
+	# Translators: Documentation for redo script.
+	script_redo.__doc__=_("Redo operation.")
+
 	def script_select_all(self, gesture):
 		if self.process_input(gesture): return
 		self.speak_appropriate_message(cm_select_all)
 
-	#Internal functions: Examines our environment so we can speak the appropriate message.
+	# Translators: Documentation for select_all script.
+	script_select_all.__doc__=_("Select all.")
+
+	# Internal functions: Examines our environment so we can speak the appropriate message.
 
 	def process_input(self, gesture):
 		gesture.send()
@@ -87,107 +106,107 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	def speak_appropriate_message(self, cm_flag):
 		cc_flag=self.examine_focus()
 
-		#Todo: If we can validate whether or not a control can work with the clipboard, we can return an invalid message here.
+		# Todo: If we can validate whether or not a control can work with the clipboard, we can return an invalid message here.
 		log.debug("Finding appropriate message for clipboard content type: %r"%cc_flag)
 		if cc_flag==cc_none: return
 
-		#Pick a word suitable to the content.
+		# Pick a word suitable to the content.
 
-		#Translators: A single word representing text.
+		# Translators: A single word representing text.
 		word=_("text")
 
 		if cc_flag==cc_file:
 
-			#Translators: A single word representing a file.
+			# Translators: A single word representing a file.
 			word=_("file")
 
 		if cc_flag==cc_list:
 
-			#Translators: A single word representing an item in a list.
+			# Translators: A single word representing an item in a list.
 			word=_("item")
 
-		#Validate and speak.
+		# Validate and speak.
 		log.debug("Validating clipboard mode: %r"%cm_flag)
 
 		if cm_flag==cm_undo and self.can_undo(cc_flag):
 
-			#Translators: Message to speak when undoing.
+			# Translators: Message to speak when undoing.
 			ui.message(_("Undo"))
 
 		if cm_flag==cm_redo and self.can_redo(cc_flag):
 
-			#Translators: A message spoken when redoing a previously undone operation.
+			# Translators: A message spoken when redoing a previously undone operation.
 			ui.message(_("Redo"))
 
 		if cm_flag==cm_select_all and self.can_select(cc_flag):
 
-			#Translators: A message to speak when selecting or highlighting all content.
+			# Translators: A message to speak when selecting or highlighting all content.
 			ui.message(_("Select All"))
 
 		if cm_flag==cm_cut and self.can_cut(cc_flag):
 
-			#Translators: A message to speak when cutting an item to the clipboard.
-			ui.message(_("Cut %s to clipboard")%word)
+			# Translators: A message to speak when cutting an item to the clipboard.
+			ui.message(_("Cut %s")%word)
 
 		if cm_flag==cm_copy and self.can_copy(cc_flag):
 			if not self.__clipboard.changed():
 
-				#Translators: A message spoken when no change has been detected on the clipboard.
+				# Translators: A message spoken when no change has been detected on the clipboard.
 				ui.message(_("No change."))
 				return
 
-			#Translators: A message spoken when copying to the clipboard.
-			ui.message(_("Copy %s to clipboard")%word)
+			# Translators: A message spoken when copying to the clipboard.
+			ui.message(_("Copy %s")%word)
 
 		if cm_flag==cm_paste and self.can_paste(cc_flag):
 
-			#Translators: A message spoken when pasting to the clipboard.
-			ui.message(_("Pasted %s from clipboard")%word)
+			# Translators: A message spoken when pasting to the clipboard.
+			ui.message(_("Pasted %s")%word)
 
 	def examine_focus(self):
 		focus=api.getFocusObject()
 		if not focus: return cc_none
 		log.debug("Examining focus object: %r"%focus)
 
-		#Check for an explorer/file browser window.
-		#Todo: Is this an accurate method?
+		# Check for an explorer/file browser window.
+		# Todo: Is this an accurate method?
 		if focus.windowClassName=="DirectUIHWND": return cc_file
 
-		#Check for a list item.
+		# Check for a list item.
 		if focus.role==ROLE_LISTITEM: return cc_list
 
-		#If we're looking at text...
+		# If we're looking at text...
 		if focus.role==ROLE_EDITABLETEXT or focus.role==ROLE_DOCUMENT:
 			log.debug("Found text control")
 
-			#Retrieve the states in case we're read-only.
+			# Retrieve the states in case we're read-only.
 			states=focus.states
 			for state in states:
 				if state==STATE_READONLY: return cc_read_only_text
 
-			#Otherwise, we're just an ordinary text field.
+			# Otherwise, we're just an ordinary text field.
 			log.debug("Field seems to be editable.")
 			return cc_text
 
-		#Comboboxes
+		# Comboboxes.
 		if focus.role==ROLE_COMBOBOX:
 			log.debug("Found combo box.")
 
-			#Again, we need to evaluate states to make sure we're editable.
+			# Again, we need to evaluate states to make sure we're editable.
 			states=focus.states
 			for state in states:
 				if state==STATE_EDITABLE: return cc_text
 
-			#Otherwise, chances are we can't copy this.
+			# Otherwise, chances are we can't copy this.
 			log.debug("Combo box content seems to be read-only.")
 			return cc_none
 
-		#Todo: Other control types we need to check?
+		# Todo: Other control types we need to check?
 		log.debug("Control type would not suggest clipboard operations.")
 		return cc_none
 
-	#Validation functions: In case we need to extend the script to allow more control/window types etc.
-	#Todo: Can we check a control to see if it enables these operations? For instance whether a list enables copy or a text field allows select all?
+	# Validation functions: In case we need to extend the script to allow more control/window types etc.
+	# Todo: Can we check a control to see if it enables these operations? For instance whether a list enables copy or a text field allows select all?
 
 	def can_undo(self, cc_flag):
 		if cc_flag==cc_read_only_text: return False
@@ -203,27 +222,27 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	def can_cut(self, cc_flag):
 		if cc_flag==cc_read_only_text: return False
 
-		#Todo: Validate the control and make sure there is something that could potentially be cut.
+		# Todo: Validate the control and make sure there is something that could potentially be cut.
 		return True
 
 	def can_copy(self, cc_flag):
 
-		#Todo: Validate the control and make sure there is something that could potentially be copied.
+		# Todo: Validate the control and make sure there is something that could potentially be copied.
 		return True
 
 	def can_paste(self, cc_flag):
 		if cc_flag==cc_read_only_text: return False
 
-		#Todo: Validate the clipboard and make sure there is something to paste.
+		# Todo: Validate the clipboard and make sure there is something to paste.
 		return True
 
-	#Define an object of type clipboard_monitor that will keep track of the clipboard for us.
+	# Define an object of type clipboard_monitor that will keep track of the clipboard for us.
 	__clipboard=clipboard_monitor.clipboard_monitor()
 
-	#Translators: Category displayed in Input Gestures configuration.
+	# Translators: Category displayed in Input Gestures configuration.
 	scriptCategory=_("Clipboard")
 
-	#Define the default gestures.
+	# Define the default gestures.
 
 	__gestures={
 		"kb:Control+A": "select_all",
@@ -235,10 +254,3 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	}
 
 
-	#Translators: Input help and documentation strings
-	script_cut.__doc__=_("Cut selected item to clipboard, if appropriate.")
-	script_copy.__doc__=_("Copy selected item to clipboard, if appropriate.")
-	script_paste.__doc__=_("Paste item from clipboard, if appropriate.")
-	script_undo.__doc__=_("Undo operation.""")
-	script_redo.__doc__=_("Redo operation.")
-	script_select_all.__doc__=_("Select all.")
